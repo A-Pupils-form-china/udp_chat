7f3229693250b3f3bbf8d2f668bd60191866df62
chuzhongzai <qq1822574439@gmail,com>
2021-05-18 16:26:58 +0800

    一个很简单的聊天器，基于udp协议，使用pysimplegui写了界面，使用threading开了一个子线程接收信息，两个文件其实是相同的，只是ip地址不一样，两个文件改成对方的ip即可使用

---
+++ udp_chat1.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+import PySimpleGUI as sg
+
+SERVER_HOST = "192.168.137.1"
+PORT = 8080
+CLIENT_HOST = "192.168.137.59"
+
+CODE = "UTF-8"
+
+
+def main():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    chat_layout = [[sg.Multiline("", size=(90, 20), k="content", disabled=True, autoscroll=True),
+                    sg.Multiline("", size=(10, 10), k="tip", disabled=True)],
+                   [sg.Multiline("", size=(45, 5), k="-in-")],
+                   [sg.B("发送"), sg.B("退出"), sg.B("skip")]]
+    chat_window = sg.Window('server', chat_layout, return_keyboard_events=True)
+
+    t1 = threading.Thread(target=recv, args=(udp_socket, chat_window))
+    t1.start()
+    global content
+    content = ""
+    while True:
+        event, value = chat_window.read()
+        if event == '发送' or event == '\r':  # 发送键或者回车
+            udp_socket.sendto(value["-in-"].replace('\n', '').replace('\r', '').encode(CODE), (SERVER_HOST, PORT))
+            chat_window['tip'].update("已发送,等待回复")
+            content += "【发送】"+value['-in-'].replace('\n', '').replace('\r', '') + ":\n"
+            chat_window['content'].update(content)
+            chat_window['-in-'].update("")
+            print("【发送】：" + value["-in-"].replace('\n', '').replace('\r', ''))
+        if event == '退出' or event == sg.WINDOW_CLOSED:
+            udp_socket.sendto("exit".encode(CODE), (CLIENT_HOST, PORT))
+            exit()
+
+
+def recv(udp_socket, window):
+    global content
+    lock = threading.Lock()
+    udp_socket.bind((CLIENT_HOST, PORT))
+    while True:
+        msg = udp_socket.recv(1000).decode(CODE)
+        if msg == "exit":
+            exit()
+        content += ("【接收】" + msg + "\n")
+        print("【接收】" + msg)
+        lock.acquire()
+        window['tip'].update('已接收信息')
+        window['content'].update(content)
+        lock.release()
+
+
+if __name__ == "__main__":
+    main()
